1b1fa33f569e0df300fcd7add5ff3cb7011f8882
rina <doroshukmaryna@gmail.com>
2024-03-20 11:01:33 +0100

    added password hashing and verification function

---
+++ Models/Passwords.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+public static class PasswordHashing
+{
+    public static string HashPassword(string password)
+    {
+        byte[] salt = new byte[128 / 8];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+        return $"{hashed}:{Convert.ToBase64String(salt)}";
+    }
+
+    public static bool VerifyPassword(string password, string storedHash)
+    {
+        var parts = storedHash.Split(':');
+        if (parts.Length != 2)
+        {
+            throw new FormatException("Unexpected hash format. Should be {HASH}:{SALT}");
+        }
+
+        var hash = parts[0];
+        var salt = Convert.FromBase64String(parts[1]);
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+        return hash == hashed;
+    }
+}
