3ce857e381237e8ad93c37f2e7e5a94b0f22e26f
Rina <doroshukmaryna@gmail.com>
2024-05-18 23:54:03 +0200

    made sure you cant got to /decks unless u r logged in

--- Components/Pages/Decks.razor.cs
+++ Components/Pages/Decks.razor.cs
@@ -7,12 +7,24 @@ namespace magick.Components.Pages;
 
 public partial class Decks {
     [Inject] public DeckService? DeckService { get; set; }
+    [Inject] public UserService? UserService { get; set; }
+    [Inject] public NavigationManager? NavManager { get; set; }
    
     private List<UserDeck> decks = [];
     
 
     protected override async Task OnInitializedAsync()
-        => await GetDecks();
+    {
+        var user = await UserService!.GetUser();
+        if (user == null)
+        {
+            NavManager!.NavigateTo("/login");
+        }
+        else
+        {
+            await GetDecks();
+        }
+    }
 
     private async Task GetDecks()
     {
